8aa91d6cb76fb914be535c561c49cd267ac81fa5
munzii <munzii422@gmail.com>
2021-11-22 23:29:03 +0900

    input error

--- src/P4344.java
+++ src/P4344.java
@@ -11,18 +11,18 @@ public class P4344 {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int n = Integer.parseInt(br.readLine());
 		double[] t = new double[n];
 		
 		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
 			int p = Integer.parseInt(st.nextToken());
-			int[] s = new int[p];
+			double[] s = new double[p];
 			int sum =0;
 			
 			for(int j=0; j<p; j++) {
-				s[j] = Integer.parseInt(st.nextToken());
+				s[j] = Double.parseDouble(st.nextToken());
 				sum+= s[j];
 			}
 			int count =0;
@@ -36,7 +36,7 @@ public class P4344 {
 			t[i] = (double)count/(double)p*100;
 		}
 		
-		for(int i=0; i<n; i++) {
+		for(int i=0; i<t.length; i++) {
 			bw.write(String.format("%.3f%%\n", t[i]));
 		}
 		
