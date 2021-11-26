cede3ea4a9b66b2f0d05c51c907590a4458d86fd
munzii <munzii422@gmail.com>
2021-11-26 23:04:49 +0900

    solving.

--- src/P4344.java
+++ src/P4344.java
@@ -13,34 +13,27 @@ public class P4344 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int n = Integer.parseInt(br.readLine());
-		double[] t = new double[n];
 		
 		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int p = Integer.parseInt(st.nextToken());
-			int[] arr = new int[p];
-			double sum =0;
-			
-			for(int j=0; j<p; j++) {
-				int s = Integer.parseInt(st.nextToken());
-				arr[j] = s;
-				sum += s;
-			}
+			st= new StringTokenizer(br.readLine(), " ");
+			int p = Integer.parseInt(br.readLine());
 			int count =0;
-			double avg = (double)sum/(double)p;
-			
-			for(int k=0; k<n; k++) {
-				if(avg<arr[k]) {
+			double t =0;
+			double[] s = new double[p];
+			for(int j=0; j<p; j++) {
+				s[j] = Integer.parseInt(st.nextToken());
+				t += s[j];
+			}
+			double avg = (double)t/(double)p;
+			for(int j=0; j<p; j++) {
+				if(s[j]>avg) {
 					count++;
 				}
 			}
-			t[i] = (double)count/(double)p*100;
+			double r = ((double)count/(double)p)*100;
+			bw.write(String.format("%.3f%%", r));
+			
 		}
-		
-		for(int i=0; i<t.length; i++) {
-			bw.write(String.format("%.3f%%\n", t[i]));
-		}
-		
 		br.close();
 		bw.flush();
 		bw.close();
